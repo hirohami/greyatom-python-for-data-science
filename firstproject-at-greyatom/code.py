d4ea51b29eeb4a2c305d9559e0bf1d2f16aae23d
hirohami <hirohami@users.noreply.github.com>
2020-08-19 07:28:31 +0530

    Code file created.

---
+++ firstproject-at-greyatom/code.py
@@ -0,0 +1,67 @@
+# --------------
+# Code starts here
+
+# Create the lists 
+class_1 = ['Geoffrey Hinton','Andrew Ng','Sebastian Raschka','Yoshua Bengio']
+class_2 = ['Hilary Manson','Carla Gentry','Corinna Cortes']
+# Concatenate both the strings
+new_class = class_1 + class_2
+print(new_class)
+# Append the list
+new_class.append('Peter Warden')
+# Print updated list
+print(new_class)
+# Remove the element from the list
+new_class.remove('Carla Gentry')
+# Print the list
+print(new_class)
+
+
+# Create the Dictionary
+
+courses = {"Math":"65","English":"70","History":"80","French":"70","Science":"60"}
+
+# Slice the dict and stores  the all subjects marks in variable
+marks = [65,70,80,70,60]
+# Store the all the subject in one variable `Total`
+
+Total = sum(marks)
+
+# Print the total
+
+print(Total)
+
+# Insert percentage formula
+
+percentage = Total * 100 / 500
+# Print the percentage
+
+print(percentage)
+
+
+# Create the Dictionary
+ 
+mathematics = {"Geoffrey Hinton":"78","Andrew Ng":"95","Sebastian Raschka":"65","Yoshua Benjio":"50","Hilary Manson":"70","Corrina Cortes":"66","Peter Warden":"75"}
+
+keyMax = max(mathematics,key = mathematics.get)
+
+print(keyMax)
+
+# Given string
+str = keyMax
+
+# Create variable first_name 
+first_name = str.split()[0]
+# Create variable Last_name and store last two element in the list
+Last_name = str.split()[1]
+# Concatenate the string
+full_name = Last_name  + ' ' + first_name
+# print the full_name
+print(full_name)
+# print the name in upper case
+certificate_name = full_name.upper()
+
+print(certificate_name)
+# Code ends here
+
+
